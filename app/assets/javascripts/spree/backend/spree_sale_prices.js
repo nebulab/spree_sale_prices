7044947229bc3819ae5aae4b1790c9f289cd2424
Matteo Latini <mtylty@gmail.com>
2016-12-07 15:14:36 +0100

    Add jquery-ui-timepicker-addon

---
+++ app/assets/javascripts/spree/backend/spree_sale_prices.js
@@ -0,0 +1,19 @@
+//= require jquery-ui-timepicker-addon
+
+handle_datetime_picker_fields = function() {
+  $('.datetimepicker').datetimepicker({
+    dateFormat: Spree.translations.date_picker,
+    dayNames: Spree.translations.abbr_day_names,
+    dayNamesMin: Spree.translations.abbr_day_names,
+    firstDay: Spree.translations.first_day,
+    monthNames: Spree.translations.month_names,
+    prevText: Spree.translations.previous,
+    nextText: Spree.translations.next,
+    showOn: "focus",
+    oneLine: true
+  });
+}
+
+$(document).ready(function() {
+  handle_datetime_picker_fields();
+});
